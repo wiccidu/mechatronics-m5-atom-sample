b8734cfe6b3a6d17ea2a5534033dc0567408e0b9
ssakoda-wiccidu <ssakoda@wiccidu.jp>
2022-08-08 03:51:02 +0900

    modify

--- advanced/sampling_adc_with_serial_bt/sampling_adc_with_serial_bt.ino
+++ advanced/sampling_adc_with_serial_bt/sampling_adc_with_serial_bt.ino
@@ -1,6 +1,14 @@
 #include <M5Atom.h>
 #include "BluetoothSerial.h"
 
+#define RED     0
+#define GREEN   1
+#define BLUE    2
+#define CYAN    3
+#define MAGENTA 4
+#define YELLOW  5
+#define WHITE   6
+
 const int SAMPLING_FREQ = 100; //[Hz]
 const int BASE_DELAY = 1000000/SAMPLING_FREQ; //[usec]
 const int AD_INPUT = 33;
@@ -12,6 +20,7 @@ void setup() {
     SerialBT.begin("ESP32");
     delay(50);
     SerialBT.println("Hello World");
+    LED(BLUE);
 }
 
 void loop() {
@@ -19,4 +28,34 @@ void loop() {
     int i = analogRead(AD_INPUT);
     SerialBT.println(i);
     delayMicroseconds(BASE_DELAY-(micros()-t));
+    M5.update();
+}
+
+void LED(int c){
+    switch (c) {
+        case 0:
+            M5.dis.drawpix(0, 0x007000);
+            break;
+        case 1:
+            M5.dis.drawpix(0, 0x700000);
+            break;
+        case 2:
+            M5.dis.drawpix(0, 0x000070);
+            break;
+        case 3:
+            M5.dis.drawpix(0, 0x700070);
+            break;
+        case 4:
+            M5.dis.drawpix(0, 0x007070);
+            break;
+        case 5:
+            M5.dis.drawpix(0, 0x707000);
+            break;
+        case 6:
+            M5.dis.drawpix(0, 0x707070);
+            break;
+        default:
+            M5.dis.drawpix(0, 0x000000);
+            break;
+    }
 }
